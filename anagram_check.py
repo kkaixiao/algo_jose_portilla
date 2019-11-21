d25ea71cc476edf2c36c6d2d73e89eea73bfd96c
Kai Xiao <show-key@qq.com>
2019-11-21 22:28:06 +0800

    just a test

--- anagram_check.py
+++ anagram_check.py
@@ -1,1 +1,14 @@
-print('ok')
+def anagram(s1, s2):
+    str1 = s1.lower().replace(' ', '')
+    str2 = s2.lower().replace(' ', '')
+    if len(str1) != len(str2):
+        return False
+
+    for i in range(len(str1)):
+        str2 = str2.replace(str1[i], '', 1)
+
+    return len(str2) == 0
+
+
+
+print(anagram('how are you', 'you arethow'))
