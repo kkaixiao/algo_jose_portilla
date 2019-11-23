54335c6595e17506c737a1bc04a26d502a8d7375
Kai Xiao <show-key@qq.com>
2019-11-24 00:58:25 +0800

    2nd commit after watching tutorial, added test case

--- find_the_miss_element_xor.py
+++ find_the_miss_element_xor.py
@@ -8,8 +8,11 @@ def finder(arr1, arr2):
     return result
 
 
-arr1 = [5, 5, 7, 7]
-arr2 = [5, 7, 7]
+arr1 = [1, 2, 3, 4, 5, 6, 7]
+arr2 = [3, 7, 2, 1, 4, 6]
+
+# arr1 = [5, 5, 7, 7]
+# arr2 = [5, 7, 7]
 print(finder(arr1, arr2))
 
 
