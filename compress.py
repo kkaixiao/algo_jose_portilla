81d16344bca17cd29d369f472285a0de6f6ef7fa
Kai Xiao <show-key@qq.com>
2019-11-25 21:10:07 +0800

    1st commit before watching tutorial

---
+++ compress.py
@@ -0,0 +1,22 @@
+
+def compress(s):
+    prechar = s[0]
+
+    i = 1
+    ret_str = ''
+    count = 1
+    while i < len(s):
+        if prechar != s[i]:
+            ret_str = ret_str + prechar + str(count)
+            count = 1
+        else:
+            count += 1
+        prechar = s[i]
+        i += 1
+    ret_str = ret_str + prechar + str(count)
+    return ret_str
+
+
+str1 = 'AAAAABBBBCCCDDDD'
+
+print(compress(str1))
